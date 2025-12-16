bbe7ad56b85bb8ea547cfe5b6c368595a690943b
harpreet-2146 <harpreetk2146@gmail.com>
2025-12-16 22:30:03 +0530

    layout: exclude navbar from landing page using route-based layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { CartProvider } from "./context/CartContext";
 import { OrderProvider } from "./context/OrderContext";
 import Navbar from "./components/Navbar";
+
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import StallMenu from "./pages/StallMenu";
@@ -11,40 +12,60 @@ import OrderStatus from "./pages/OrderStatus";
 import VendorHome from "./pages/VendorHome";
 import VendorDashboard from "./pages/VendorDashboard";
 
+/* -------- Layouts -------- */
+
+const LandingLayout = () => (
+  <div className="min-h-screen bg-base-100">
+    <Outlet />
+  </div>
+);
+
+const MainLayout = () => (
+  <div className="min-h-screen bg-base-100">
+    <Navbar />
+    <main className="pt-20 pb-10 px-4">
+      <Outlet />
+    </main>
+  </div>
+);
+
 function App() {
   return (
     <BrowserRouter>
       <CartProvider>
         <OrderProvider>
-          <div className="min-h-screen bg-gradient-to-br from-orange-50 via-white to-pink-50">
-            <Navbar />
-            <main className="pt-20 pb-10 px-4">
-              <Routes>
-                {/* Landing */}
-                <Route path="/" element={<LandingPage />} />
 
-                {/* Customer Routes */}
-                <Route path="/customer" element={<Home />} />
-                <Route path="/stall/:stallId" element={<StallMenu />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/order/:orderId" element={<OrderStatus />} />
+          <Routes>
+            {/* Landing (NO navbar) */}
+            <Route element={<LandingLayout />}>
+              <Route path="/" element={<LandingPage />} />
+            </Route>
 
-                {/* Vendor Routes */}
-                <Route path="/vendor" element={<VendorHome />} />
-                <Route path="/vendor/:stallId" element={<VendorDashboard />} />
-              </Routes>
-            </main>
-            <Toaster
-              position="bottom-center"
-              toastOptions={{
-                style: {
-                  background: "#333",
-                  color: "#fff",
-                  borderRadius: "100px",
-                },
-              }}
-            />
-          </div>
+            {/* App routes (WITH navbar) */}
+            <Route element={<MainLayout />}>
+              {/* Customer */}
+              <Route path="/customer" element={<Home />} />
+              <Route path="/stall/:stallId" element={<StallMenu />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/order/:orderId" element={<OrderStatus />} />
+
+              {/* Vendor */}
+              <Route path="/vendor" element={<VendorHome />} />
+              <Route path="/vendor/:stallId" element={<VendorDashboard />} />
+            </Route>
+          </Routes>
+
+          <Toaster
+            position="bottom-center"
+            toastOptions={{
+              style: {
+                background: "hsl(var(--b2))",
+                color: "hsl(var(--bc))",
+                borderRadius: "999px",
+              },
+            }}
+          />
+
         </OrderProvider>
       </CartProvider>
     </BrowserRouter>
